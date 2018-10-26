715dcb24724aded82826fc679ce7930e1d08c8bb
Charles Machalow <csm10495@gmail.com>
2018-10-25 22:10:45 -0700

    Add simple cpp compilation/run tests

--- static/tests/test_container.py
+++ static/tests/test_container.py
@@ -1,6 +1,21 @@
 import docker
 import os
 
+SIMPLE_CPP_SOURCE = '''
+#include <string>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+    int a = 5;
+    std::cout << "Five: " << a << " / " << std::to_string(a) << std::endl;
+
+    return 0;
+}
+
+'''
+
 def test_volume_mount(bc):
     bc.cmd('touch /mnt/cwd/test')
     os.remove(os.path.join(os.getcwd(), 'test'))
@@ -27,3 +42,29 @@ def test_curl_version(bc):
 def test_openssl_version(bc):
     output = bc.cmd('openssl version')
     assert '1.1.1' in output
+
+def test_simple_cpp_exe_old_abi(bc):
+    with open("test.cpp", 'w') as f:
+        f.write(SIMPLE_CPP_SOURCE)
+
+    bc.cmd('g++ -std=c++17 -D_GLIBCXX_USE_CXX11_ABI=0 /mnt/cwd/test.cpp -o /mnt/cwd/test')
+    bc.cmd('chmod +x /mnt/cwd/test')
+    bc.cmd('/mnt/cwd/test')
+
+    assert os.system('./test') == 0
+
+    os.remove('test.cpp')
+    os.remove('test')
+
+def test_simple_cpp_exe_static_cpp(bc):
+    with open("test.cpp", 'w') as f:
+        f.write(SIMPLE_CPP_SOURCE)
+
+    bc.cmd('g++ -std=c++17 -static-libstdc++ /mnt/cwd/test.cpp -o /mnt/cwd/test')
+    bc.cmd('chmod +x /mnt/cwd/test')
+    bc.cmd('/mnt/cwd/test')
+
+    assert os.system('./test') == 0
+
+    os.remove('test.cpp')
+    os.remove('test')
